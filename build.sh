7d8e6722cd6d43d446b7699a19f75bfc0b8eb4a9
rodobre <52138375+rodobre@users.noreply.github.com>
2020-04-04 09:42:33 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 javac -d ./bin/ -cp "./:/usr/share/java/jna.jar:/usr/share/java/jna-platform.jar" sysjail/ArgumentParser.java sysjail/LogType.java sysjail/Logger.java sysjail/NativeInterfaceConnector.java sysjail/Main.java
 cd bin && jar cvfmP ../builds/sysjail.jar ../META-INF/MANIFEST.MF -C . sysjail/
-ln ./builds/sysjail.jar /bin/sysjail.jar
+ln ../builds/sysjail.jar /bin/sysjail.jar
